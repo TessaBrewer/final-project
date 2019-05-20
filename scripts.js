1a70b5cb1796be01171b85c79541f7a8f488c3eb
highqualitybean <32654356+highqualitybean@users.noreply.github.com>
2019-05-20 10:25:09 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,10 +8,14 @@ var booksID =
   {"id": "OL6552298M"}, //The Raven
   {"id": "OL9831606M"}, //The Case of Charles Dexter Ward
   {"id": "OL24372171M"}, // At the Mountains of Madness
-  {"id": "OL24205740M"}, //The Dunwich Horror
+  {"id": "OL24205740M"} //The Dunwich Horror
 ]
 
 function printMyBooks(myArray)
 {
   myArray.forEach(x => console.log(x));
 }
+
+booksID.forEach(
+  book => book.smallPhoto = "http://covers.openlibrary.org/b/olid/" + book[id] + "-S.jpg";
+);

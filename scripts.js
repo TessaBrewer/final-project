040b063282bd3895b53c16be292f8310de07a43a
highqualitybean <32654356+highqualitybean@users.noreply.github.com>
2019-05-20 10:08:54 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
-console.log("TEST");
+var booksID =
+[
+  {"id": "OL24326648M"}, //frankenstein
+  {"id": "OL7120857M"}, //Dr.Jekyl and Mr.Hyde
+  {"id": "OL2550667M"}, //It
+  {"id": "OL24207120M"}, //Firestarter
+  {"id": "OL7033786M"}, //Edgar Allan Poe
+  {"id": "OL6552298M"}, //The Raven
+  {"id": "OL9831606M"}, //The Case of Charles Dexter Ward
+  {"id": "OL24372171M"}, // At the Mountains of Madness
+  {"id": "OL24205740M"}, //The Dunwich Horror
+]
+
+function printMyBooks(myArray)
+{
+  myArray.forEach(x => console.log(x));
+}

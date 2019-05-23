33743b22fe0ac5ccdabde43849d4fe7e3329d2ec
highqualitybean <32654356+highqualitybean@users.noreply.github.com>
2019-05-22 20:40:04 -0400

    sync ajax requests and finish book list

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const featuredProduct = 3;//index of the featured product, change this to change the featured product
+const featuredProduct = 12;//index of the featured product, change this to change the featured product
 
 var booksID =
 [
@@ -11,6 +11,22 @@ var booksID =
   {"olid": "OL9831606M"}, //The Case of Charles Dexter Ward
   {"olid": "OL24372171M"}, // At the Mountains of Madness
   {"olid": "OL24205740M"}, //The Dunwich Horror
+  {"olid": "OL26341628M"}, //The World is Blue
+  {"olid": "OL26338814M"}, //Russian book? (I hope its pg D-:)
+  {"olid": "OL9427026M"}, //Medical Insurance Handbook
+  {"olid": "OL17196313M"}, //Lord of the flies
+  {"olid": "OL22976416M"}, //More Poe :D
+  {"olid": "OL21856932M"}, //Taoist Yoga?
+  {"olid": "OL23213727M"}, //War and Peace
+  {"olid": "OL7577028M"}, //War of the Worlds
+  {"olid": "OL24382006M"}, //1984
+  {"olid": "OL17240074M"}, //brave new world
+  {"olid": "OL22064758M"}, //Writings of Rosa Luxemburg
+  {"olid": "OL25083117M"}, //Communist Manifesto
+  {"olid": "OL5541887M"}, //Das Kapital
+  {"olid": "OL5478196M"}, //American History
+  {"olid": "OL7110112M"}, //weird satan poem
+
 ]
 
 function printMyBooks(myArray) //nice for debugging
@@ -20,7 +36,8 @@ function printMyBooks(myArray) //nice for debugging
 
 var books = []; //asynchronicity >:(              @TODO: kill async
 
-booksID.forEach(x =>
+booksID.forEach(function(x)
+{
   {
     var urlToBeCalled = 'https://openlibrary.org/api/books?bibkeys=OLID:' + x.olid + '&jscmd=details&format=json';
     $.ajax(
@@ -36,9 +53,13 @@ booksID.forEach(x =>
       {
         console.log(x.olid + " request failed");
         console.log(jqXHR, textStatus, errorThrow, urlToBeCalled);
-      }
+      },
+      async: false
     })
   }
+}
 ); //this could've been done better, but it works so ¯\_(ツ)_/¯
 
-document.getElementById("featuredProductName").innerHTML = books[3].details.title;
+document.getElementsByClassName("featuredProductName")[0].innerHTML = books[featuredProduct].details.title;
+document.getElementsByClassName("featuredProductDescription")[0].innerHTML = "By " + books[featuredProduct].details.authors[0].name;
+document.getElementsByClassName("featuredProductImage")[0].setAttribute("src", books[featuredProduct].thumbnail_url);

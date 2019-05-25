9a4feed10a30a3904c083595d1d301e04b5efa79
highqualitybean <32654356+highqualitybean@users.noreply.github.com>
2019-05-24 23:48:36 -0400

    selectProduct desktop layout

--- scripts.js
+++ scripts.js
@@ -6,30 +6,30 @@ var currentProduct = sessionStorage.getItem("passedProductValue"); //should stor
 
 var booksID =
 [
-  {"olid": "OL24326648M", price: 6}, //frankenstein
-  {"olid": "OL7120857M", price: 16}, //Dr.Jekyl and Mr.Hyde
-  {"olid": "OL2550667M", price: 59}, //It
-  {"olid": "OL24207120M", price: 16}, //Firestarter
-  {"olid": "OL7033786M", price: 14}, //Edgar Allan Poe
-  {"olid": "OL6552298M", price: 5}, //The Raven
-  {"olid": "OL9831606M", price: 16}, //The Case of Charles Dexter Ward
-  {"olid": "OL24372171M", price: 14}, // At the Mountains of Madness
-  {"olid": "OL24205740M", price: 12}, //The Dunwich Horror
-  {"olid": "OL26341628M", price: 147}, //The World is Blue
-  {"olid": "OL26338814M", price: 1}, //Russian book? (I hope its pg D-:)
-  {"olid": "OL9427026M", price: 21}, //Medical Insurance Handbook
-  {"olid": "OL17196313M", price: 14}, //Lord of the flies
-  {"olid": "OL22976416M", price: 13}, //More Poe :D
-  {"olid": "OL21856932M", price: 5}, //Taoist Yoga?
-  {"olid": "OL23213727M", price: 6}, //War and Peace
-  {"olid": "OL7577028M", price: 9}, //War of the Worlds
-  {"olid": "OL24382006M", price: 4}, //1984
-  {"olid": "OL17240074M", price: 3}, //brave new world
-  {"olid": "OL22064758M", price: 20}, //Writings of Rosa Luxemburg
-  {"olid": "OL25083117M", price: 89}, //Communist Manifesto
-  {"olid": "OL5541887M", price: 6}, //Das Kapital
-  {"olid": "OL5478196M", price: 8}, //American History
-  {"olid": "OL7110112M", price: 3}, //weird satan poem
+  {"olid": "OL24326648M", price: "6.00"}, //frankenstein
+  {"olid": "OL7120857M", price: "16.60"}, //Dr.Jekyl and Mr.Hyde
+  {"olid": "OL2550667M", price: "59.21"}, //It
+  {"olid": "OL24207120M", price: "16.00"}, //Firestarter
+  {"olid": "OL7033786M", price: "14.41"}, //Edgar Allan Poe
+  {"olid": "OL6552298M", price: "5.30"}, //The Raven
+  {"olid": "OL9831606M", price: "16.00"}, //The Case of Charles Dexter Ward
+  {"olid": "OL24372171M", price: "14.36"}, // At the Mountains of Madness
+  {"olid": "OL24205740M", price: "12.99"}, //The Dunwich Horror
+  {"olid": "OL26341628M", price: "147.99"}, //The World is Blue
+  {"olid": "OL26338814M", price: "1.23"}, //Russian book? (I hope its pg D-:)
+  {"olid": "OL9427026M", price: "21.00"}, //Medical Insurance Handbook
+  {"olid": "OL17196313M", price: "14.50"}, //Lord of the flies
+  {"olid": "OL22976416M", price: "13.00"}, //More Poe :D
+  {"olid": "OL21856932M", price: "5.00"}, //Taoist Yoga?
+  {"olid": "OL23213727M", price: "6.00"}, //War and Peace
+  {"olid": "OL7577028M", price: "9.99"}, //War of the Worlds
+  {"olid": "OL24382006M", price: "4.25"}, //1984
+  {"olid": "OL17240074M", price: "3.21"}, //brave new world
+  {"olid": "OL22064758M", price: "20.02"}, //Writings of Rosa Luxemburg
+  {"olid": "OL25083117M", price: "89.25"}, //Communist Manifesto
+  {"olid": "OL5541887M", price: "6.25"}, //Das Kapital
+  {"olid": "OL5478196M", price: "8.25"}, //American History
+  {"olid": "OL7110112M", price: "3.36"}, //weird satan poem
 
 ]
 
@@ -63,6 +63,38 @@ booksID.forEach(function(x)
   }
 }
 ); //this could've been done better, but it works so ¯\_(ツ)_/¯
+
+function getImage(olid, size) //Open library doesn't always have the images I need, so I want to use a placeholder image
+{
+  var myUrl = "http://covers.openlibrary.org/b/olid/" + olid + "-" + size + ".jpg"; //add ?default=false after the fix so that it returns a 404 when the image is missing
+/*
+  $.ajax({
+    type: "GET",
+    url: myUrl,
+    datatype: "xml",
+    error: function(jqXHR, textStatus, errorThrown) {
+        if (jqXHR.status == 404) {
+          switch(size)
+          {
+            default:
+            case "S":
+              return "https://via.placeholder.com/40x60";
+            case "M":
+              return "https://via.placeholer.com/180x281";
+            case "L":
+              return "https://via.placeholder.com/320x500";
+          }
+        }
+    },
+    success: function()
+    {
+      return myUrl;
+    }
+  });
+  */ //TODO FIX THIS SO THAT IT RETURNS A PLACEHOLDER IMAGE IN THE EVENT THAT THE ORIGINAL IMAGE IS MISSING
+
+  return myUrl; //REMOVE THIS AFTER THE FIX 
+}
 
 function goToSingleProduct() //always store the "passedProductValue" in sessionStorage before calling this
 {
@@ -103,7 +135,7 @@ document.getElementById("shoppingCartButton").addEventListener("click", function
 function fillExampleProduct(x, y) //@params x: class name, y: product number //for use of the main page
 {
   document.getElementsByClassName(x)[0].innerHTML = 
-  "<img src=" + "http://covers.openlibrary.org/b/olid/" + booksID[y].olid + "-M.jpg" + ">"
+  "<img src=" + getImage(booksID[y].olid, "M") + ">"
   + "<h2 class=\"clearText\">" + books[y].details.title + "</h2>"
   + "<h2 class=\"clearText\"> $" + booksID[y].price + "</h2>";
   document.getElementsByClassName(x)[0].addEventListener("click", function()
@@ -114,13 +146,22 @@ function fillExampleProduct(x, y) //@params x: class name, y: product number //f
   });
 }
 
+function fillSelectProductExtraProducts(x)
+{
+  var randomProductNumber = Math.floor(Math.random() * books.length);
+  document.getElementsByClassName(x)[0].innerHTML = 
+  "<img class=\"extraSelectProductImage\" src=" + getImage(booksID[randomProductNumber].olid, "M") + ">" +
+  "<h3 class=\"extraSelectProductName\">" + books[randomProductNumber].details.title + "</h3>" +
+  "<p class=\"extraSelectProductPrice\">$" + booksID[randomProductNumber].price + "</p>";
+}
+
 if(fileName[0] == "mainPage.html") //put mainpage only code here (mostly stuff that populates the page)
 {
   console.log("This should only run on the main page");
 
   document.getElementsByClassName("featuredProductName")[0].innerHTML = books[featuredProduct].details.title;
   document.getElementsByClassName("featuredProductDescription")[0].innerHTML = "By " + books[featuredProduct].details.authors[0].name;
-  document.getElementsByClassName("featuredProductImage")[0].setAttribute("src", "http://covers.openlibrary.org/b/olid/" + booksID[featuredProduct].olid + "-M.jpg");
+  document.getElementsByClassName("featuredProductImage")[0].setAttribute("src", getImage(booksID[featuredProduct].olid, "M"));
   document.getElementsByClassName("learnMoreFeaturedProduct")[0].addEventListener("click", function()
   {
     sessionStorage.setItem("passedProductValue", featuredProduct);
@@ -134,6 +175,19 @@ if(fileName[0] == "mainPage.html") //put mainpage only code here (mostly stuff t
   //sessionStorage.setItem("passedProductValue", 0);
 }
 
-if(fileName[0] == "singleProductPage") //put singleProduct only code here (stuff that populates the single product page)
+if(fileName[0] == "singleProductPage.html") //put singleProduct only code here (stuff that populates the single product page)
 {
+  console.log("This should only run on the single product page");
+
+  document.getElementById("selectProductImage").setAttribute("src", getImage(booksID[currentProduct].olid, "L"));
+  document.getElementsByClassName("selectProductName")[0].innerHTML = "<h1>" + books[currentProduct].details.title + "</h1>";
+  document.getElementsByClassName("selectProductDescription")[0].innerHTML = 
+  books[currentProduct].details.authors[0].name + 
+  "<br>Published " + books[currentProduct].details.publish_date +
+  "<br>" + books[currentProduct].details.number_of_pages + " p.";
+  document.getElementsByClassName("selectProductPrice")[0].innerHTML = "$" + booksID[currentProduct].price;
+
+  fillSelectProductExtraProducts("firstSelectProduct");
+  fillSelectProductExtraProducts("secondSelectProduct");
+  fillSelectProductExtraProducts("thirdSelectProduct");
 }
